0be6840cb23749e304531152a8c550a009cdea9e
PhamVanThanhG <robertthanh1107@gmail.com>
2022-03-21 13:09:19 +0700

    Message

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{page?}', function ($page="home"){
     return view($page);
 });
+
+// Route::get('/home', function() {
+//     return view('home');
+// });
+
+// Route::get('/welcome', function() {
+//     return view('welcome');
+// });
+
+// Route::get('/about?name={name}&{pass}', function($age) {
+//     return view('about');
+// })->middleware('checklogin');
